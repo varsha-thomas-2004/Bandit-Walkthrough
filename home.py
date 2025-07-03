a58e7827390c1119a43f0a9818736e7d4e0f9ac2
Varsha Thomas <varshathomas364@gmail.com>
2025-07-03 19:52:25 +0530

    Improve Landing Page
    Add image and reordering of some sentences

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 #st.set_page_config(layout="centered")
-st.title("The Final Nudge! 🫠")
+st.title("The Final Nudge! 💣")
 st.write("## Walkthrough: OverTheWire - Bandit 🥷")
 st.write("### ⚠️ Only come here if you feel absolutely stuck! ⚠️")
-st.write("Pull the sidebar for accessing the walkthrough for each level.")
+st.image("catoverthewire.gif", caption="Be like this catto!!! 😼")
+st.write("*Access the sidebar for the walkthrough for each level.")
